e0cf2cccfcae3100bd12c2fa50ce9ffab3f3a4ea
Doherty254 <kiptoodickson908@gmail.com>
2022-06-14 09:34:13 +0300

    added 404 route

--- app.js
+++ app.js
@@ -10,12 +10,26 @@ app.use(express.static('public'))
 
 // index route (root path)
 app.get('/', (req, res) => {
-    res.render('index')
+    let title = 'Home'
+    res.render('index', {title: title})
 })
 
 // list route
 app.get('/list', (req, res) => {
-    res.render('list')
+    let title = 'List'
+    const list = [
+        'Wake up',
+        'Read a book',
+        'Take a shower',
+        'Prepare breakfast',
+        'Open VS Code'
+    ]
+    res.render('list', {title: title, list: list})
+})
+
+// 404 Error
+app.get('*',(req,res) => {
+    res.render('404')
 })
 
 // start server on localhost:3000
