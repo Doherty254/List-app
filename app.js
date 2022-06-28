49d05b2168c639d28a946819b6ecffce3f27eadc
Doherty254 <kiptoodickson908@gmail.com>
2022-06-28 09:46:43 +0300

    list update

--- app.js
+++ app.js
@@ -8,6 +8,19 @@ app.set('view engine', 'ejs')
 // source static files from public folder
 app.use(express.static('public'))
 
+// configuration to access form input when submitted
+app.use(express.urlencoded({extended: false}))
+
+
+// LIST OF ITEMS
+const list = [
+    'Brown Bread',
+    '2Kgs of Sugar',
+    'Coffee',
+    'Fresh Fri Cooking Oil',
+    'Soko Ugali - 10Kgs'
+]
+
 // index route (root path)
 app.get('/', (req, res) => {
     let title = 'Home'
@@ -17,20 +30,21 @@ app.get('/', (req, res) => {
 // list route
 app.get('/list', (req, res) => {
     let title = 'List'
-    const list = [
-        'Brown Bread',
-        '2Kgs of Sugar',
-        'Coffee',
-        'Fresh Fri Cooking Oil',
-        'Soko Ugali - 10Kgs'
-    ]
+  
     res.render('list', {title: title, list: list})
+})
+
+// ADD ITEM TO LIST
+app.post('/add-item', (req,res) => {
+    let item = req.body.item
+    list.push(item)
+    res.redirect('/list')
 })
 
 // 404 Error
 app.get('*',(req,res) => {
     let title = 'Page Not Found'
-    res.render('404')
+    res.render('404', {title:title})
 })
 
 // start server on localhost:3000
