34f7a8aa17eaa5d54ab869691f2fada41c4397c3
Doherty254 <kiptoodickson908@gmail.com>
2022-06-29 11:20:47 +0300

    changes

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 import express from 'express'
+import mysql from 'mysql'
 
 const app = express()
+
+// db configuration
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'list_app'
+})
 
 // set template engine and provide extension
 app.set('view engine', 'ejs')
@@ -13,13 +22,13 @@ app.use(express.urlencoded({extended: false}))
 
 
 // LIST OF ITEMS
-const list = [
-    'Brown Bread',
-    '2Kgs of Sugar',
-    'Coffee',
-    'Fresh Fri Cooking Oil',
-    'Soko Ugali - 10Kgs'
-]
+// const list = [
+//     'Brown Bread',
+//     '2Kgs of Sugar',
+//     'Coffee',
+//     'Fresh Fri Cooking Oil',
+//     'Soko Ugali - 10Kgs'
+// ]
 
 // index route (root path)
 app.get('/', (req, res) => {
@@ -31,14 +40,76 @@ app.get('/', (req, res) => {
 app.get('/list', (req, res) => {
     let title = 'List'
   
-    res.render('list', {title: title, list: list})
+    let sql = 'SELECT * FROM list'
+
+    connection.query(
+        sql, (error, results) => {
+            res.render('list', {title: title, list: results})
+        }
+    )
+
+    
 })
 
 // ADD ITEM TO LIST
 app.post('/add-item', (req,res) => {
     let item = req.body.item
-    list.push(item)
-    res.redirect('/list')
+
+    let sql = 'INSERT INTO list (item) VALUES (?)'
+
+    connection.query(
+        sql,
+        [item],
+        (error, results) => {
+            res.redirect('/list')
+        }
+    )
+})
+
+// DISPLAY EDIT ITEM FORM
+
+app.get('/edit/:id', (req, res) => {
+
+    let sql = 'SELECT * FROM list WHERE id = ?'
+
+    connection.query(
+        sql,
+        [parseInt(req.params.id)],
+        (error, result) => {
+            res.render('edit', {list: result[0], title: 'Edit Item'})
+        }
+    )
+})
+    
+
+// SUBMITTING EDIT ITEM FORM
+app.post('/edit/:id', (req, res) => {
+    
+    let item = req.body.item
+
+    let sql ='UPDATE list SET item = ? WHERE id = ?'
+
+    connection.query(
+        sql,
+        [item, parseInt(req.params.id)],
+        (error, result) => {
+            res.redirect('/list')
+        }
+    )
+
+})
+
+// DELETE ITEM FROM LIST
+app.get('/delete/:id', (req, res) =>{
+    let sql = 'DELETE FROM list WHERE id = ?'
+
+    connection.query(
+        sql,
+        [parseInt(req.params.id)],
+        (error, results) => {
+            res.redirect('/list')
+        }
+    )
 })
 
 // 404 Error
